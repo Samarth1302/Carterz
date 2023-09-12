ade8aa3240d5e31ee4486c9655f5ad8e70a989b4
Samarth Chauhan <samarth.chauhan13022002@gmail.com>
2023-09-12 14:32:36 +0530

    css

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <div>
-      <footer className="text-white bg-blue-500 body-font">
+      <footer className="text-white bg-blue-950 body-font">
         <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
           <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
             <Link href={"/"} legacyBehavior>
@@ -55,7 +55,7 @@ const Footer = () => {
             </div>
 
             <div className="lg:w-1/3 w-full px-4">
-              <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+              <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">
                 POLICY
               </h2>
               <nav className="list-none mb-10">
@@ -86,7 +86,7 @@ const Footer = () => {
               </nav>
             </div>
             <div className="lg:w-1/3 w-full px-4">
-              <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+              <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">
                 SERVICES
               </h2>
               <nav className="list-none mb-10">
@@ -110,13 +110,13 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="bg-gray-100">
+        <div className="bg-white">
           <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-            <p className="text-gray-500 text-sm text-center sm:text-left">
+            <p className="text-black text-sm text-center sm:text-left">
               © 2023 Carterz (Samarth)
             </p>
             <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-              <Link href={"/"} className="text-gray-500">
+              <Link href={"/"} className="text-gray-800">
                 <svg
                   fill="currentColor"
                   strokeLinecap="round"
@@ -128,7 +128,7 @@ const Footer = () => {
                   <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                 </svg>
               </Link>
-              <Link href={"/"} className="ml-3 text-gray-500">
+              <Link href={"/"} className="ml-3 text-gray-800">
                 <svg
                   fill="currentColor"
                   strokeLinecap="round"
@@ -140,7 +140,7 @@ const Footer = () => {
                   <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                 </svg>
               </Link>
-              <Link href={"/"} className="ml-3 text-gray-500">
+              <Link href={"/"} className="ml-3 text-gray-800">
                 <svg
                   fill="none"
                   stroke="currentColor"
@@ -154,7 +154,7 @@ const Footer = () => {
                   <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
                 </svg>
               </Link>
-              <Link href={"/"} className="ml-3 text-gray-500">
+              <Link href={"/"} className="ml-3 text-gray-800">
                 <svg
                   fill="currentColor"
                   stroke="currentColor"
